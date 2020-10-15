eb49c694d68b642d15dfdd3dabf194a3f29b70ee
jiseop9083 <64767436+jiseop9083@users.noreply.github.com>
2020-10-15 13:22:01 +0900

    Create q2261.cpp

---
+++ divide and conquer/q2261.cpp
@@ -0,0 +1,68 @@
+//백준 2261번 가장 가까운 두 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<pair<int, int>> ve;//y, x
+int an;
+
+int dis(pair<int, int> a, pair<int, int> b) {
+	return ((a.first - b.first) * (a.first - b.first)) + ((a.second - b.second) * (a.second - b.second));
+}
+
+bool comX(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second) {
+		return a.first < b.first;
+	}
+	return a.second < b.second;
+}
+
+bool comY(pair<int, int> a, pair<int, int> b) {
+	if (a.first == b.first) {
+		return a.second < b.second;
+	}
+	return a.first < b.first;
+}
+
+int divide_conquer(int start, int end) {//start와 end는 점의 번호
+	if (start + 1 == end) {
+		return dis(ve[start], ve[end]);
+	}
+	else if (start + 2 == end) {
+		return min(dis(ve[start], ve[end]),min(dis(ve[start], ve[start + 1]) ,dis(ve[start + 1], ve[end])) );
+	}
+	int mid = (start + end) / 2;
+	int line = (ve[mid].second + ve[mid + 1].second) / 2;
+	int ret = min(divide_conquer(start, mid), divide_conquer(mid + 1, end));
+	vector<pair<int, int>> mid_ve;
+	for (int i = start; i <= end; i++) {
+		if ((line - ve[i].second) * (line - ve[i].second) < ret) {
+			mid_ve.push_back(ve[i]);
+		}
+	}
+	sort(mid_ve.begin(), mid_ve.end(), comY);
+	for (int i = 0; i < mid_ve.size(); i++) {
+		for (int j = i + 1; j < mid_ve.size() &&
+			(mid_ve[j].first - mid_ve[i].first) * (mid_ve[j].first - mid_ve[i].first) < ret; j++) {
+			ret = min(ret, dis(mid_ve[i], mid_ve[j]));
+		}
+	}
+	return ret;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int a, b; cin >> a >> b;
+		ve.push_back({ b, a });
+	}
+	sort(ve.begin(), ve.end(), comX);
+	cout << divide_conquer(0, n - 1);
+	return 0;
+}
